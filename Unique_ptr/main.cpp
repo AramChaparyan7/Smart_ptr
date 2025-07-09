75dbc08fe848532b806ae02075002428d2114a90
aram <aram@Ubuntu.myguest.virtualbox.org>
2025-07-09 15:37:41 +0400

    Unique_ptr

---
+++ Unique_ptr/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <utility>
+#include <gtest/gtest.h>
+
+template<typename T>
+class Unique_ptr{
+private:
+    T* ptr;
+public:
+    explicit Unique_ptr(T* p = nullptr) : ptr{p} {}
+
+    Unique_ptr(const Unique_ptr&) = delete;
+
+    Unique_ptr& operator=(const Unique_ptr&) = delete; 
+
+    Unique_ptr(Unique_ptr&& other) noexcept{
+        ptr = other.ptr;
+        other.ptr = nullptr;
+    }
+
+    Unique_ptr& operator=(Unique_ptr&& other) noexcept{
+        if(this == &other){
+            return *this;
+        }
+        delete ptr;
+        ptr = other.ptr;
+        other.ptr = nullptr;
+        return *this;
+    }  
+    
+    T& operator*(){
+        return *ptr;
+    }
+
+    T* operator->(){
+        return ptr;
+    }
+
+    T* get(){
+        return ptr;
+    }
+
+    T* release(){
+        T* tmp = ptr;
+        ptr = nullptr;
+        return tmp;
+    }
+
+    void reset(T* p = nullptr){
+        delete ptr;
+        ptr = p;
+    }
+
+    explicit operator bool(){
+        return ptr?true:false;
+    }
+};
+
+int main(int argc, char** argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+    return 0;
+}
+
+TEST(UniquePtrTest, Dereference) {
+    Unique_ptr<int> ptr(new int(10));
+    EXPECT_EQ(*ptr, 10);
+    EXPECT_EQ(*ptr, 10);
+}
+
+TEST(UniquePtrTest, MoveConstructor) {
+    Unique_ptr<int> ptr1(new int(20));
+    Unique_ptr<int> ptr2(std::move(ptr1));
+    EXPECT_EQ(*ptr2, 20);
+    EXPECT_EQ(ptr1.get(), nullptr); 
+}
+
+TEST(UniquePtrTest, MoveAssignment) {
+    Unique_ptr<char> ptr1(new char('a'));
+    Unique_ptr<char> ptr2;
+    ptr2 = std::move(ptr1);
+    EXPECT_EQ(*ptr2, 'a');
+    EXPECT_EQ(ptr1.get(), nullptr);
+}
+
+TEST(UniquePtrTest, ResetReleasesOldObject) {
+    Unique_ptr<double> ptr(new double(40.5));
+    ptr.reset(new double(50.9));
+    EXPECT_EQ(*ptr, 50.9);
+}
